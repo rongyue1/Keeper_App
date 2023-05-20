ee1a275efeb69168389e40d0735bdff5e2265725
Rong Sun <sun.rongyue@outlook.com>
2023-05-20 15:47:35 +1200

    adding comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,11 @@ import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
 function App() {
   const [notes, setNotes] = useState([]);
 
@@ -13,6 +18,10 @@ function App() {
     });
   }
 
+  //2. Implement the delete note functionality.
+  //- Callback from the Note component to trigger a delete function.
+  //- Use the filter function to filter out the item that needs deletion.
+  //- Pass a id over to the Note component, pass it back to the App when deleting.
   function deletePost(id) {
     setNotes((prevNotes) => {
       return prevNotes.filter((note, index) => index !== id);
